ab5b3f4d203cad417e1836f038a9677146c84f54
@Bipul Rahi <91903346+BipulRahi@users.noreply.github.com>
2023-02-11 23:52:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
-function myfuction(){
-    let bi=document.getElementById("bi");
-    setInterval(()=>{
-        let bi=document.getElementById("bi");
-        bi.style.display=none;
-    })
-}
+// function myfuction(){
+//     let bi=document.getElementById("bi");
+//     setInterval(()=>{
+//         let bi=document.getElementById("bi");
+//         bi.style.display=none;
+//     })
+// }
